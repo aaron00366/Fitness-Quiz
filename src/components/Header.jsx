6528d86c6eca89087e7c14bd4bd4edfd0ab88e3b
Shih Aaron <shihaaron@ShihdeMacBook-Air.local>
2025-05-02 16:16:33 +0800

    fix vite.config

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header>
       <img src={logoImg} alt="Quiz logo" />
-      <h1>Fitness Quiz</h1>
+      <h1>健身知識小測驗</h1>
     </header>
   );
 }
--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -18,19 +18,19 @@ export default function Summary({ userAnswers }) {
   return (
     <div id="summary">
       <img src={quizCompleteImg} alt="Trophy icon" />
-      <h2>Quiz Completed!</h2>
+      <h2>完成!</h2>
       <div id="summary-stats">
         <p>
           <span className="number">{skippedAnswersShare}%</span>
-          <span className="text">skipped</span>
+          <span className="text">跳過</span>
         </p>
         <p>
           <span className="number">{correctAnswersShare}%</span>
-          <span className="text">answered correctly</span>
+          <span className="text">答對</span>
         </p>
         <p>
           <span className="number">{wrongAnswersShare}%</span>
-          <span className="text">answered incorrectly</span>
+          <span className="text">答錯</span>
         </p>
       </div>
       <ol>

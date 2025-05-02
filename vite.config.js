8bcc6145fdb918d51d13440c92142fa43b33276b
Shih Aaron <shihaaron@ShihdeMacBook-Air.local>
2025-05-02 16:04:36 +0800

    fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Fitness-Quiz/',
   plugins: [react()],
-  base: '/fitness-quiz/',
 })
